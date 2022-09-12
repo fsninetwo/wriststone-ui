d1aa6eb4d53f49079576887e11e2ce994f62bcdb
Никита Медведев <grizzlyFS92@hotmail.com>
2022-09-12 10:38:38 +0300

    Main components initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Wriststone/home/home.component';
+import { ProductsComponent } from './Wriststone/products/products.component';
+import { ProfileComponent } from './Wriststone/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Wriststone/home/home.component';
+import { ProfileComponent } from './Wriststone/profile/profile.component';
+import { ProductComponent } from './Wriststone/products/product/product.component';
+import { ProductsComponent } from './Wriststone/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ProductComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,

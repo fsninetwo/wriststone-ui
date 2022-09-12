3e058a698eab42b8998dff787bbbaa8e45c46435
Никита Медведев <grizzlyFS92@hotmail.com>
2022-09-12 13:31:12 +0300

    Initialize store page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './Wriststone/footer/footer.component';
 import { UserModule } from './wriststone/user/user.module';
-import { UserDetailComponent } from './Wriststone/user/user-detail/user-detail.component';
+import { FooterComponent } from './wriststone/footer/footer.component';
+import { AuthModule } from './wriststone/auth/auth.module';
+import { StoreComponent } from './Wriststone/store/store.component';
+import { CategoryComponent } from './Wriststone/store/category/category.component';
+import { ItemComponent } from './Wriststone/store/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    UserDetailComponent,
+    StoreComponent,
+    CategoryComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule
+    UserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]

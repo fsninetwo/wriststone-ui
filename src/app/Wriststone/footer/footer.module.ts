0ef22a04efd20427b10d39385f90a9cfd60fa5f8
Никита Медведев <grizzlyFS92@hotmail.com>
2022-09-12 13:29:57 +0300

    Delete some footer links

---
+++ src/app/Wriststone/footer/footer.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './footer.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent
+  ],
+})
+export class FooterModule { }

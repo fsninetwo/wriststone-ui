11b48ac0b6a5e85180b59f28d119912fb09ac7ff
Никита Медведев <grizzlyFS92@hotmail.com>
2022-09-12 12:54:03 +0300

    delete login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Wriststone/login/login/login.component';
-import { LogoutComponent } from './Wriststone/login/logout/logout.component';
-import { SignupComponent } from './Wriststone/login/signup/signup.component';
 import { ProductsComponent } from './Wriststone/products/products.component';
-import { ProfileComponent } from './Wriststone/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'user', loadChildren: () => import('./Wriststone/user/user.module').then(m => m.UserModule)  },
+  { path: 'store', component: ProductsComponent },
+  { path: '', redirectTo: '/store', pathMatch: 'full'  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './Wriststone/profile/profile.component';
 import { ProductComponent } from './Wriststone/products/product/product.component';
 import { ProductsComponent } from './Wriststone/products/products.component';
-import { LoginComponent } from './Wriststone/login/login/login.component';
-import { LogoutComponent } from './Wriststone/login/logout/logout.component';
-import { SignupComponent } from './Wriststone/login/signup/signup.component';
 import { FooterComponent } from './Wriststone/footer/footer.component';
+import { UserModule } from './Wriststone/user/user.module';
+import { UserDetailComponent } from './Wriststone/user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
     ProductComponent,
     ProductsComponent,
-    LoginComponent,
-    LogoutComponent,
-    SignupComponent,
-    FooterComponent
+    FooterComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

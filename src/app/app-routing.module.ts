50bb26a816847dd9eb58ad338e8c203c2ddc00b1
Никита Медведев <grizzlyFS92@hotmail.com>
2022-09-12 13:01:41 +0300

    Remove produce, change namings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './Wriststone/products/products.component';
 
 const routes: Routes = [
-  { path: 'user', loadChildren: () => import('./Wriststone/user/user.module').then(m => m.UserModule)  },
-  { path: 'store', component: ProductsComponent },
+  { path: 'user', loadChildren: () => import('./wriststone/user/user.module').then(m => m.UserModule)  },
   { path: '', redirectTo: '/store', pathMatch: 'full'  },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './Wriststone/products/product/product.component';
-import { ProductsComponent } from './Wriststone/products/products.component';
 import { FooterComponent } from './Wriststone/footer/footer.component';
-import { UserModule } from './Wriststone/user/user.module';
+import { UserModule } from './wriststone/user/user.module';
 import { UserDetailComponent } from './Wriststone/user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    ProductsComponent,
     FooterComponent,
     UserDetailComponent,
   ],

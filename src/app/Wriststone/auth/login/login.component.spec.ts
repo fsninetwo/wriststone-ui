0246861c1e51b2bb73978da6675100d217ec99c5
Никита Медведев <grizzlyFS92@hotmail.com>
2023-11-16 09:22:15 +0300

    update dependencies for login component

--- src/app/Wriststone/auth/login/login.component.spec.ts
+++ src/app/Wriststone/auth/login/login.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthInfoService } from 'src/app/services/auth/auth-info.service';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 import { LoginComponent } from './login.component';
+
+export class NavigationServiceMock {
+
+}
+
+export class AuthInfoServiceMock {
+
+}
+
+export class AuthServiceMock {
+
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +23,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [],
+      providers: [
+        { provide: NavigationService, useClass: NavigationServiceMock },
+        { provide: AuthInfoService, useClass: AuthInfoServiceMock },
+        { provide: AuthService, useClass: AuthServiceMock },
+      ]
     })
     .compileComponents();
   });

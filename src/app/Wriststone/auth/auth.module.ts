a6dae74e92e55492843e440c97b239ed8ed6a5f4
Никита Медведев <grizzlyFS92@hotmail.com>
2023-11-29 15:21:29 +0300

    fix jwt dependency

--- src/app/Wriststone/auth/auth.module.ts
+++ src/app/Wriststone/auth/auth.module.ts
@@ -1,15 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { JwtModule } from '@auth0/angular-jwt';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
-export function tokenGetter() {
-  return localStorage.getItem("access_token");
-}
 
 @NgModule({
   declarations: [
@@ -17,12 +12,6 @@ export function tokenGetter() {
     SignupComponent
   ],
   imports: [
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        allowedDomains: ["localhost:4200"],
-      },
-    }),
      AuthRoutingModule,
      ReactiveFormsModule,
      CommonModule

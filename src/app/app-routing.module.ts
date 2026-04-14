6fe8b4364828c5c622ff58bd2b19eb0b6d47033c
Mikita Miadzvedzeu <mikita.miadzvedzeu@ventionteams.com>
2026-04-14 11:53:23 +0300

    move back to fix issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('../wriststone/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'user', loadChildren: () => import('../wriststone/user/user.module').then(m => m.UserModule) },
-  { path: 'users', loadChildren: () => import('../wriststone/users-management/users-management.module').then(m => m.UsersManagementModule) },
-  { path: 'store', loadChildren: () => import('../wriststone/store/store.module').then(m => m.StorePageModule) },
+  { path: 'auth', loadChildren: () => import('./wriststone/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'user', loadChildren: () => import('./wriststone/user/user.module').then(m => m.UserModule) },
+  { path: 'users', loadChildren: () => import('./wriststone/users-management/users-management.module').then(m => m.UsersManagementModule) },
+  { path: 'store', loadChildren: () => import('./wriststone/store/store.module').then(m => m.StorePageModule) },
   { path: '', redirectTo: '/store/list', pathMatch: 'full'  },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { UserModule } from '../wriststone/user/user.module';
-import { FooterComponent } from '../wriststone/footer/footer.component';
-import { AuthModule } from '../wriststone/auth/auth.module';
-import { HeaderComponent } from '../wriststone/header/header.component';
+import { UserModule } from './wriststone/user/user.module';
+import { FooterComponent } from './wriststone/footer/footer.component';
+import { AuthModule } from './wriststone/auth/auth.module';
+import { HeaderComponent } from './wriststone/header/header.component';
 import { AuthGuard } from './services/auth/auth.guard';
-import { UsersManagementModule } from '../wriststone/users-management/users-management.module';
+import { UsersManagementModule } from './wriststone/users-management/users-management.module';
 import { InputTextComponent } from './shared/base-input-text/base-input-text.component';
 import { JwtModule } from '@auth0/angular-jwt';
 import { AuthInterceptor } from './core/interceptors/auth-interceptor.service';
-import { StorePageModule } from '../wriststone/store/store.module';
+import { StorePageModule } from './wriststone/store/store.module';
 import { AppStateModule } from './store/app.module';
 
 export function tokenGetter() {
